544df5de788fb95293f8ba464e72a196db431354
KJafro <Kurtj222@outlook.com>
2022-12-23 10:20:40 +0000

    Register media queries

--- recipes/components/Navbar.js
+++ recipes/components/Navbar.js
@@ -7,10 +7,10 @@ function Navbar() {
     <nav className={styles.navbar}>
         <ul className={styles.ul}>
         <div className={styles.navleft}>
-            <li><Link href='/createrecipe'>Recipes</Link></li>
-            <li><Link href='/home'>Chefs</Link></li>
-            <li><Link href='/home'>Reviews</Link></li>
-            <li><Link href='/home'>Suprise Me</Link></li>
+            <li><Link href='/'>Recipes</Link></li>
+            <li><Link href='/'>Chefs</Link></li>
+            <li><Link href='/'>Reviews</Link></li>
+            <li><Link href='/'>Suprise Me</Link></li>
             </div>
             <div className={styles.navright}>
             <li><Link href='/login'>Login</Link></li>
