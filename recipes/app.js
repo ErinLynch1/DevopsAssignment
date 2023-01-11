e12d386e2a7193d0e6e012de7a1316ff72500488
eltonczlon123 <5STEPK21@solent.ac.uk>
2023-01-11 00:55:52 +0000

    Database connection/schemas

--- recipes/app.js
+++ recipes/app.js
@@ -1,12 +1,29 @@
 const bodyParser = require('body-parser');
 const express = require('express');
-const expressLayouts = require('express-ejs-layouts')
+const expressLayouts = require('express-ejs-layouts');
+const { default: mongoose } = require('mongoose');
 const app = express();
 const port = process.env.port || 3000;
 require('dotenv').config();
 app.use(express.urlencoded( { extended: true } ));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
+
+mongoose.set('strictQuery', false);
+
+mongoose.connect("mongodb+srv://admin:admin@recipesdev.xjq6sp2.mongodb.net/RecipesDev?retryWrites=true&w=majority", {
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+});
+
+const db = mongoose.connection
+db.on('error', (err) =>{
+    console.log(err)
+})
+
+db.once('open', () => {
+    console.log('Database connection successful!')
+})
 
 app.get("/", (req, res) => {res.render("index.ejs")});
 app.get("/register", (req, res) => {res.render("register.ejs")});
--- recipes/seeder.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb+srv://admin:admin@recipesdev.xjq6sp2.mongodb.net/RecipesDev?retryWrites=true&w=majority", {
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-});
-
-module.exports = mongoose;
